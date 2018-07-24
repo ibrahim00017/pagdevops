84c62f62096b13825031958f8258b797a7be1ddb
ibrahim00017 <hibrahimyya@gmail.com>
2018-07-24 19:25:51 +0100

    dd

--- bootcamp-deploy-war.sh
+++ bootcamp-deploy-war.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #premier parametre representant le repertoire dans lequel ont a cloner tous les projets
-# repository_dir=$1
+repository_dir=$1
 
 #Un tableau contenant le nom de tous les projets qui generent un war
 projets_wars=("categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel"  "debat.service.fonctionnel"  "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "notification.diffusion.services" "preference.service.fonctionnel" "programme.service.fonctionnel" )
@@ -15,8 +15,8 @@ projets_wars=("categorie.service.fonctionnel" "projet.service.fonctionnel" "comm
 
 for projet_war in ${projets_wars[@]}
    do
-	# if [ -d "$repository_dir" ]
-	# then
+	if [ -d "$repository_dir" ]
+	then
 	      cd "$projet_war"
 	      rm -rf src/main/resources/local/
 	      rm -rf src/main/resources/dev/
@@ -24,7 +24,8 @@ for projet_war in ${projets_wars[@]}
 	      rm -rf src/test/resources/dev/
 	      rm -rf target/ log*
 	      mvn clean install -P test -DskipTests
-	      nohup mvn spring-boot:run -P test  -DskipTests > log.out &
+	      # nohup mvn spring-boot:run -P test  -DskipTests > log.out &
 	      mvn spring-boot:run -P test  -DskipTests > log.out 
+	fi
 	
 done
