9ea621d5d7e10fc0b6d590303926fb346cd065b5
ibrahim00017 <hibrahimyya@gmail.com>
2018-07-31 15:43:55 +0100

    optimisation

---
+++ push origin release_2.0.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#premier parametre representant le repertoire dans lequel ont a cloner tous les projets
+#Un tableau contenant le nom de tous les projets qui generent un war
+msg=$1
+projets_wars=("categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel"  "debat.service.fonctionnel"  "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "notification.diffusion.services" "preference.service.fonctionnel" "programme.service.fonctionnel" "debat.service.use.case")
+
+#On verifie si le repertoire dans lequel ont a cloner tous les projets existe
+#Et On sort du script si le repertoire n'existe
+# if [ ! -d "$repository_dir" ]
+# then
+#     exit
+# fi
+
+
+for projet_war in ${projets_wars[@]}
+   do
+	
+	      cd "$projet_war"
+	      git add .
+	      git commit -m "$msg"
+	      git push origin release_2.0
+	      cd ..
+	
+	
+done
